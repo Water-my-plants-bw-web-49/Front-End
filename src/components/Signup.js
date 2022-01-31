a2bb683c29466a6e3c3402b6007ff343cfb5a759
adamhinton <adam.d.hinton@gmail.com>
2022-01-31 16:51:08 -0500

    copied in components and other files from before

---
+++ src/components/Signup.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+
+export default class SignUp extends Component {
+  render() {
+    return (
+      <form>
+        <h3>Register</h3>
+
+        <div className="form-group">
+          <label>user name</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="create user name"
+          />
+        </div>
+
+        <div className="form-group">
+          <label>Phone</label>
+          <input
+            type="phone number"
+            className="form-control"
+            placeholder="Enter Phone Number"
+          />
+        </div>
+
+        <div className="form-group">
+          <label>Password</label>
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Enter password"
+          />
+        </div>
+
+        <button type="submit" className="btn btn-dark btn-lg btn-block">
+          Register
+        </button>
+        <p className="forgot-password text-right">
+          Already registered <a href="#">log in?</a>
+        </p>
+      </form>
+    );
+  }
+}
