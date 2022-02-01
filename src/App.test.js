ee0eea24d6b42b405c40c29ce1f80633945a1dd2
Sam Bezzant <sambezzant@Sams-MacBook-Pro.local>
2022-02-01 02:48:35 -0800

    separated login test and used screen

--- src/App.test.js
+++ src/App.test.js
@@ -5,11 +5,3 @@ import App from "./App";
 test("renders App without errors", () => {
   render(<App />);
 });
-
-test("renders the login header"),
-  () => {
-    const { getByText } = render(<App />);
-    const header = getByText(/login to water your plants/i);
-
-    expect(header).toBeTruthy();
-  };
