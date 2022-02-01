8971df891a50d73e1030ae3191926fd84ab856ae
Sam Bezzant <sambezzant@Sams-MacBook-Pro.local>
2022-02-01 02:20:01 -0800

    login header test

--- src/App.test.js
+++ src/App.test.js
@@ -5,3 +5,9 @@ import App from "./App";
 test("renders App without errors", () => {
   render(<App />);
 });
+
+test("renders the login header"),
+  () => {
+    const { getByText } = render(<App />);
+    const header = getByText(/login to water your plants/i);
+  };
