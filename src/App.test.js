242efa0caf657d0ba0fb20eac32a4e623d40e3c0
Sam Bezzant <sambezzant@Sams-MacBook-Pro.local>
2022-02-02 04:30:19 -0800

    test adjusted and passing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("water your plants", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerElement = screen.getByText(/water your plants home/i);
+  expect(headerElement).toBeTruthy();
 });
